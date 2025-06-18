b9224c4db875f40adf951d3dd7bd167a55fbd4f8
Temuulen <9temuulen9@gmail.com>
2025-06-18 22:04:41 +0800

    add backend

---
+++ backend/src/app.schema.ts
@@ -0,0 +1,17 @@
+
+// import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+// import { Document } from 'mongoose';
+
+// @Schema()
+// export class User extends Document {
+//   @Prop({ required: true })
+//   name: string;
+
+//   @Prop()
+//   email: string;
+
+//   @Prop()
+//   age: number;
+// }
+
+// export const UserSchema = SchemaFactory.createForClass(User);
